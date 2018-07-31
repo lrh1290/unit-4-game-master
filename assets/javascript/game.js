a038e8c38b71ba6f76683d46ac9c356534e8e8dc
lrh1290 <38796653+lrh1290@users.noreply.github.com>
2018-07-31 00:09:59 -0500

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,146 +1,119 @@
-// Characters
-var frodo = {
-    name: 'Frodo',
-    id: 'frodo',
-    health: 150,
-    attack: 20,
-    counterAttack: 25
-};
-
-var merry = {
-    name: 'Merry',
-    id: 'merry',
-    health: 75,
-    attack: 10,
-    counterAttack: 20
-}
-
-var pippin = {
-    name: 'Pippin',
-    id: 'pippin',
-    health: 100,
-    attack: 25,
-    counterAttack: 30
-};
-
-var sam = {
-    name: 'Sam',
-    id: 'sam',
-    health: 100,
-    attack: 25,
-    counterAttack: 30
-}
-
 var attacker = {};
 var defender = {};
 var attackerSelected = false;
 var defenderSelected = false;
 var attackerHealth = 0;
 var defenderHealth = 0;
-
+var frodo = {
+    name: 'Frodo',
+    id: 'frodo',
+    health: 150,
+    ap: 20,
+    cp: 25
+};
+var merry = {
+    name: 'Merry',
+    id: 'merry',
+    health: 75,
+    ap: 10,
+    cp: 20
+}
+var pippin = {
+    name: 'Pippin',
+    id: 'pippin',
+    health: 100,
+    ap: 25,
+    cp: 30
+};
+var sam = {
+    name: 'Sam',
+    id: 'sam',
+    health: 100,
+    ap: 25,
+    cp: 30
+}
 // Choose a character
 function chooseAttacker(chosenCharacter) {
     attacker.name = chosenCharacter.name;
     attacker.id = chosenCharacter.id;
     attacker.health = chosenCharacter.health;
-    attacker.attack = chosenCharacter.attack;
-    attacker.counterAttack = chosenCharacter.counterAttack;
+    attacker.ap = chosenCharacter.ap;
+    attacker.cp = chosenCharacter.cp;
     attackerSelected = true;
 }
-
-
 function chooseDefender(chosenCharacter) {
     defender.name = chosenCharacter.name;
     defender.id = chosenCharacter.id;
     defender.health = chosenCharacter.health;
-    defender.attack = chosenCharacter.attack;
-    defender.counterAttack = chosenCharacter.counterAttack;
+    defender.ap = chosenCharacter.ap;
+    defender.cp = chosenCharacter.cp;
     defenderSelected = true;
 }
-
 function updateHealth() {
-    attackerHealth = attacker.health - defender.attack;
+    attackerHealth = attacker.health - defender.ap;
     attacker.health = attackerHealth;
-
-    defenderHealth = defender.health - attacker.attack;
+    defenderHealth = defender.health - attacker.ap;
     defender.health = defenderHealth;
-    attacker.attack += attacker.counterAttack;
+    attacker.ap += attacker.cp;
 
     if (attacker.id == 'frodo') {
         $("#frodo-health").text(attackerHealth);
-    } else if (attacker.id == 'merry') {
-        $("#merry-health").text(attackerHealth);
-    } else if (attacker.id == 'pippin') {
-        $("#pippin-health").text(attackerHealth);
     } else if (attacker.id == 'sam') {
         $("#sam-health").text(attackerHealth);
     };
-
     if (defender.id == 'frodo') {
         $("#frodo-health").text(defenderHealth);
-    } else if (defender.id == 'merry') {
-        $("#merry-health").text(defenderHealth);
-    } else if (defender.id == 'pippin') {
-        $("#pippin-health").text(defenderHealth);
     } else if (defender.id == 'sam') {
         $("#sam-health").text(defenderHealth);
     };
 };
-
 function fight() {
     if (defender.health > 0) {
         updateHealth();
     } else {
         if (defender.id == 'frodo') {
             $("#frodo").hide();
-        } else if (defender.id == 'merry') {
-            $("#merry").hide();
-        } else if (defender.id == 'pippin') {
-            $("#pippin").hide();
         } else if (defender.id == 'sam') {
             $("#sam").hide();
         };
         defenderSelected = false;
     };
 };
-
-// Document.ready
+function intializeHealth() {
+    //find characters in html with jquery and store variable
+    var fighters = $(".fighters");
+    //loop through fighters
+    for (var i = 0; i < fighters.length; i++) {
+    //get the id 
+    var fighter = fighters[i].getAttribute("id");
+    //get hp
+    var gethp = fighter.health;
+    //display hp
+    $("#" + fighter[i] + "> .health").textContent(gethp);
+    }
+}
 $(document).ready(function () {
-
-    // On clicking a character
-    $(".character-box").on("click", function () {
-
+    $(".fighters").on("click", function () {
         if ($(this).attr("id") == "frodo") {
-            var userChoice = frodo;
-        } else if ($(this).attr("id") == "merry") {
-            var userChoice = merry;
-        } else if ($(this).attr("id") == "pippin") {
-            var userChoice = pippin;
+            var selection = frodo;
         } else if ($(this).attr("id") == "sam") {
-            var userChoice = sam;
+            var selection = sam;
         };
-
         if (!attackerSelected) {
             $(this).appendTo("#your-character");
             $(this).hide();
             $(this).css("display", "block");
-            console.log("Character: " + userChoice.name);
-            chooseAttacker(userChoice);
+            console.log("Character: " + selection.name);
+            chooseAttacker(selection);
         } else if (!defenderSelected) {
             $(this).appendTo("#enemy-character");
             $(this).hide();
             $(this).css("display", "block");
-            console.log("Enemy: " + userChoice.name);           
-            chooseDefender(userChoice);
+            console.log("Enemy: " + selection.name);           
+            chooseDefender(selection);
         };
     });
-
-    // On clicking "attack"
     $(".btn").on("click", function() {
-        
         fight();
-
-
     });
-
 });
